b2a27d733c6961cd333514fde1aa1e6e89fde900
Gaspare <Gas9220@users.noreply.github.com>
2023-04-17 19:49:07 +0200

    Added sample accessories and foods

--- Models/Accessory.php
+++ Models/Accessory.php
@@ -13,3 +13,27 @@ class Accessory extends Product implements \JsonSerializable {
         return get_object_vars($this);
     }
 }
+
+$sampleAccessory1 = new Accessory(
+    "Scratch Playpost",
+    2,
+    'Cat-N',
+    "Playpost Cat Scratcher with Sisal post, square base and hanging play ball with catnip and carpet base.",
+    "29x29x39 5kg",
+    [Pet::cat],
+    "https://www.petshop.co.uk/SCA%20Product%20Images/Playpost-Cat-N-Scratch-Ball-29x29x39cm_1?resizeid=5&resizeh=1200&resizew=1200",
+    ["Wood, tissue"]
+);
+
+$sampleAccessory2 = new Accessory(
+    "Wild Bird Seed Feeder",
+    0,
+    'Kingdom',
+    "This Bird Seed Feeder is manufactured from durable plastic and is easy to use and hang. Simply fill with bird seed and hang with the attached hanging rope.",
+    "5x15x5 1kg",
+    [Pet::bird],
+    "https://www.petshop.co.uk/SCA%20Product%20Images/Wild-Bird-Seed-Feeder-with-Birds-Perch_1.png?resizeid=5&resizeh=1200&resizew=1200",
+    ["Plastic"]
+);
+
+var_dump($sampleAccessory1);
--- Models/Food.php
+++ Models/Food.php
@@ -15,3 +15,25 @@ class Food extends Product implements \JsonSerializable
         return get_object_vars($this);
     }
 }
+
+$sampleFood1 = new Food(
+    "Maxi Adult",
+    10,
+    "Royal Canin",
+    "The nutritional requirements of a larger breed dog versus a smaller breed dog vary greatly, therefore it is essential you feed a balanced diet according to size and other particular sensitivities. ROYAL CANIN® Maxi Adult food is specially formulated with your dog's nutritional needs in mind. This food is suitable for large dogs aged 15 months and over - with an adult weight ranging between 26-44kg.",
+    "22x50x5 12kg",
+    [Pet::dog],
+    "Dehydrated poultry protein, maize, maize flour, animal fats, wheat, dehydrated pork protein, rice, hydrolysed animal proteins, maize gluten, beet pulp, minerals, fish oil, soya oil, yeasts and parts thereof, hydrolysed crustaceans, hydrolysed cartilage.",
+    "https://shop-cdn-m.mediazs.com/bilder/royal/canin/maxi/adult/crocchette/per/cani/8/400/80729_pla_royalcanin_maxiadult_15kg_hs_01_8.jpg"
+);
+
+$sampleFood2 = new Food(
+    "Kitten Loaf In Sauce",
+    10,
+    "Royal Canin",
+    "When your kitten is around 4 months old, their growth rate might slow but their activity levels are on the up and up! It's a time when your kitten needs the right nutrients to support developing bones, muscles, organs, and immune and nervous systems. Very soon, your kitten will also develop 30 adult teeth.",
+    "12x22x3 5kg",
+    [Pet::cat],
+    "Meat and animal derivatives, vegetable protein extracts, derivatives of vegetable origin, cereals, minerals, vegetables, oils and fats, yeasts",
+    "https://shop-cdn-m.mediazs.com/bilder/royal/canin/kitten/in/salsa/alimento/umido/per/gatti/3/400/rc_spt_wet_kitten_cig_mv_3_it_it_3.jpg"
+);
